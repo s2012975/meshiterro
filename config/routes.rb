1ef3a1fb68bcef36042d2b4b4535f9d75c840ea3
EC2 Default User <ec2-user@ip-172-31-38-69.ap-northeast-1.compute.internal>
2021-03-05 21:33:05 +0000

    [add]コメント機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   root 'post_images#index'
   
   devise_for :users
-  resources :post_images, only: [:new, :create, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show] do
+    resource :post_comments, only: [:create, :destroy]
+  end
+  
 end
