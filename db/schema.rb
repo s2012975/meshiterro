87cceb2cfe031849d361646d11e72cd86c92ecf1
EC2 Default User <ec2-user@ip-172-31-38-69.ap-northeast-1.compute.internal>
2021-03-06 22:00:53 +0000

    [add]userの編集ページ

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_06_003647) do
+ActiveRecord::Schema.define(version: 2021_03_06_213645) do
 
   create_table "customers", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2021_03_06_003647) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "profile_image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
